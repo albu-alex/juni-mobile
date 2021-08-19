7b91d45f0c89066cdbf791825844f74c4fcd8207
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-20 00:15:31 +0300

    infinite scrolling implemented successfully

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 
 <script>
 import StatusBar from "./components/StatusBar";
+import Vue from 'vue';
 import PageLayout from "./components/PageLayout";
 
 export default {
@@ -22,6 +23,36 @@ export default {
     StatusBar,
     PageLayout
   },
+  mounted(){
+    this.scroll();
+  },
+  methods:{
+    scroll(){
+      window.onscroll = () => {
+        const PageLayoutConstructor = Vue.extend(PageLayout);
+        let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
+
+        if (bottomOfWindow) {
+          setTimeout(function () {
+            for (let i = 0; i < 2; i++) {
+              let postsScrolled = document.querySelector("ul");
+              let listElement = document.createElement("li");
+              listElement.setAttribute('id', 'post');
+              new PageLayoutConstructor({
+                propsData:{
+                  text:"and this is the second post",
+                  imageLink:"https://procoders.tech/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2021/07/03a203deee86e43d13f4a61ec66459b2.png.webp",
+                  avatar:"https://www.der-windows-papst.de/wp-content/uploads/2019/03/Windows-Change-Default-Avatar-448x400.png",
+                  username:"OP_rah"
+                }
+              }).$mount('#post');
+              postsScrolled.appendChild(listElement);
+            }
+          }, 100);
+        }
+      }
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,30 +5,4 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  data:{
-    loading: false,
-  },
-  mounted(){
-    this.scroll();
-  },
-  methods:{
-    scroll(){
-      window.onscroll = () => {
-        let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
-
-        if (bottomOfWindow) {
-          setTimeout(function () {
-            for (var i = 0; i < 2; i++) {
-              let postsScrolled = document.querySelector("ul");
-              var listElement = document.createElement("li");
-              var post = document.createTextNode("HEY THERE");
-              listElement.appendChild(post);
-              postsScrolled.appendChild(listElement);
-            }
-            this.loading = false;
-          }, 100);
-        }
-      }
-    }
-  }
 }).$mount('#app')
