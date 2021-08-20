e440c8a6168075a6fecb1f2074ad0b3b1b1d3fdc
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-20 10:08:47 +0300

    optimizing posts

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@ export default {
         let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
 
         if (bottomOfWindow) {
+          let listElements = document.querySelectorAll("li");
           setTimeout(function () {
             for (let i = 0; i < 2; i++) {
               let postsScrolled = document.querySelector("ul");
@@ -40,7 +41,7 @@ export default {
               listElement.setAttribute('id', 'post');
               new PageLayoutConstructor({
                 propsData:{
-                  text:"and this is the second post",
+                  text:"This is post number " + (listElements.length + 1),
                   imageLink:"https://procoders.tech/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2021/07/03a203deee86e43d13f4a61ec66459b2.png.webp",
                   avatar:"https://www.der-windows-papst.de/wp-content/uploads/2019/03/Windows-Change-Default-Avatar-448x400.png",
                   username:"OP_rah"
