374431c08ad6fa8d62a883db5a2a9f672880f1f7
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-22 19:26:21 +0300

    all data is now fetched from randomuser.me/api

--- src/AppScript.vue
+++ src/AppScript.vue
@@ -31,27 +31,12 @@ export default {
     },
     scroll(){
       window.onscroll = () => {
-        const PageLayoutConstructor = Vue.extend(PageLayout);
         let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
         if (bottomOfWindow) {
-          setTimeout(function () {
-            let listElements = document.querySelectorAll("li");
-            for (let i = 0; i < 6; i++) {
-              let postsScrolled = document.querySelector("ul");
-              let listElement = document.createElement("li");
-              let elementID = "post" + (listElements.length+1);
-              listElement.setAttribute('id', elementID);
-              new PageLayoutConstructor({
-                propsData:{
-                  text:"This is post number " + (listElements.length + 1),
-                  imageLink:["https://procoders.tech/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2021/07/03a203deee86e43d13f4a61ec66459b2.png.webp"],
-                  avatar:"https://www.der-windows-papst.de/wp-content/uploads/2019/03/Windows-Change-Default-Avatar-448x400.png",
-                  username:"OP_rah"
-                }
-              }).$mount('#'+elementID);
-              postsScrolled.appendChild(listElement);
-            }
-          }, 100);
+            axios.get(`https://randomuser.me/api/?results=2`).then(response => {
+              for(let i=0;i<response.data.results.length;i++)
+                this.users.push(response.data.results[i]);
+          });
         }
       }
     }
