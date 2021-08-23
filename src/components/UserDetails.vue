ac16d6e75e89941c8ba6f03482c7ee66f75bafd0
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-08-23 19:13:27 +0300

    distinctive owner avatar

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -21,12 +21,15 @@
     border: 3px solid black;
   }
   img#owner{
+    --x: 50%;
+    --y: 50%;
     display: inline-block;
     border-radius: 50%;
     width: 50px;
     height: 50px;
     margin: 0 10px;
-    border: 3px solid red;
+    border: 3px solid transparent;
+    background: linear-gradient(#000, #000) padding-box, radial-gradient(farthest-corner at var(--x) var(--y), red, blue) border-box;
   }
   h1{
     display: inline-block;
